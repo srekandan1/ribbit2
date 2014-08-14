1ddad562f78a011993c9ac2adc8d4fb153be4fea
srekandan1 <srekandan@rediff.com>
2014-08-14 10:15:26 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ribbit2::Application.config.secret_token = '836c37209df3c054589b98b9da3827813832638bf767b38aff9724deddaccb2c303688e9331b462dc337eb7259d97b7dfb7f3f8c1653d71ddb78739a93f26653'
